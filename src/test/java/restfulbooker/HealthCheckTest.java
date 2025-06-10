b42fd6782c76f5c7f9b7e3a4dd151510c759db67
NKravchenko <nataliebayarea@gmail.com>
2025-06-10 22:24:14 +0200

    failed test

--- src/test/java/restfulbooker/HealthCheckTest.java
+++ src/test/java/restfulbooker/HealthCheckTest.java
@@ -22,7 +22,7 @@ public class HealthCheckTest extends BaseTest {
                 statusCode(201);
     }
 
-    //@Test
+    @Test
     public void headersAndCookiesTest() {
         Header someHeader = new Header("some_name", "Some_value");
         spec.header(someHeader);
